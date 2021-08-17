cfbaf1a6f37959b278a4fe4b440b80b4138b6131
sjbartl <steveb87@gmail.com>
2021-08-17 18:36:53 +0100

    added and removed photos

--- src/video.js
+++ src/video.js
@@ -2,8 +2,8 @@ const constraints = {
   audio: false,
   video: {
     mandatory: {
-      minWidth: 1280,
-      minHeight: 800,
+      minWidth: 853,
+      minHeight: 480,
       maxWidth: 1280,
       maxHeight: 800
     }
